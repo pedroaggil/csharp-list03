fc3ab4174fa6e2c2ed876be1cc75372a63014399
Pedro Gil <pedroaggil@gmail.com>
2023-04-11 01:20:30 -0300

    Atualizar o Program.cs

--- ex__06/Program.cs
+++ ex__06/Program.cs
@@ -1,4 +1,4 @@
-﻿// System issues
+﻿// Escreva um programa que calcule e exiba a média de 10 notas digitadas pelo usuário. Considerar notas válidas, somente valores entre 0 (zero) e 10 (dez)
 
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,27 @@ namespace ex__06
     {
         static void main(string[] args)
         {
-            // Code...
+            int average, sum = 0, mention;
+
+                for (int i = 1; i <= 10; i++)
+                {
+
+                    do
+                    {
+                        Console.Write("Insira a {0}° menção: ", i);
+                        mention = int.Parse(Console.ReadLine());
+
+                        if (mention < 0 || mention > 10)
+                        {
+                            Console.WriteLine("Insira valores válidos (entre 0 e 10), por favor.");
+                        }
+
+                    } while (mention < 0 || mention > 10);
+                    sum = sum + mention;
+                }
+
+                average = sum / 10;
+                Console.WriteLine("A média final do indivíduo avaliado é: " + media);
 
             Console.ReadKey();
         }
