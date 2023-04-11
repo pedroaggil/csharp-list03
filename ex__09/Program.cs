bb55006b862fcbdea5d73ffcfabb03778e6ca449
Pedro Gil <pedroaggil@gmail.com>
2023-04-11 12:44:56 -0300

    Atualizar o Program.cs

--- ex__09/Program.cs
+++ ex__09/Program.cs
@@ -1,4 +1,4 @@
-﻿// System issues
+﻿// Escreva um programa que leia vários números inteiros e ao final informe quantos números pares, quantos números ímpares, quantos números positivos e quantos números negativos foram digitados pelo usuário
 
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,41 @@ namespace ex__09
     {
         static void main(string[] args)
         {
-            // Code...
+            int number, par = 0, imp = 0, pos = 0, neg = 0;
+                string res = "S";
+
+                do
+                {
+                    Console.Write("Digite um número: ");
+                    number = int.Parse(Console.ReadLine());
+
+                    if (number % 2 == 0)
+                    {
+                        par++;
+                    }
+                    else
+                    {
+                        imp++;
+                    }
+
+                    if (number >= 0)
+                    {
+                        pos++;
+                    }
+                    else
+                    {
+                        neg++;
+                    }
+
+                    Console.Write("Deseja encerrar o programa (S/N)? ");
+                    res = Console.ReadLine();
+
+                } while (res != "S" && res != "s");
+
+                Console.WriteLine("Pares: " + par);
+                Console.WriteLine("Ímpares: " + imp);
+                Console.WriteLine("Positivos: " + pos);
+                Console.WriteLine("Negativos: " + neg);
 
             Console.ReadKey();
         }
