b97167b6fc9a01b8997518d03b1dc4e240989ed4
Pedro Gil <pedroaggil@gmail.com>
2023-04-10 21:43:20 -0300

    Update Program.cs

--- ex__03/Program.cs
+++ ex__03/Program.cs
@@ -1,6 +1,10 @@
-﻿// System issues
+﻿// Escreva um programa que informe a tabuada de um número a ser digitado pelo usuário
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ex__03
 {
@@ -8,7 +12,16 @@ namespace ex__03
     {
         static void main(string[] args)
         {
-            // Code...
+            Console.Write("Digite um número para que lhe seja mostrado sua tabuada (de 1 a 10): ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", number, i, number * i);
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
