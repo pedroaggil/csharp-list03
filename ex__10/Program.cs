3c753865112cb5382c7f665fadf9141e6f4f8c75
Pedro Gil <pedroaggil@gmail.com>
2023-04-11 12:59:00 -0300

    Atualizar o Program.cs

--- ex__10/Program.cs
+++ ex__10/Program.cs
@@ -1,4 +1,4 @@
-﻿// System issues
+﻿// Faça um programa que leia as variáveis C e N, respectivamente código e número de horas trabalhadas de um operário. Calcule o salário sabendo-se que ele ganha R$ 10,00 por hora. Quando o número de horas exceder a 50, calcule o excesso de pagamento armazenando-o na variável E, caso contrário zerar tal variável. A hora excedente de trabalho vale R$ 20,00. No final do processamento imprimir o salário total e o salário excedente
 
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,40 @@ namespace ex__10
     {
         static void main(string[] args)
         {
-            // Code...
+            int c, n, e = 0, sal, exc;
+                string res;
+
+                do
+                {
+                    Console.Write("Digite o código do operário: ");
+                    c = int.Parse(Console.ReadLine());
+
+                    Console.Write("Digite a quantidade de horas trabalhadas: ");
+                    n = int.Parse(Console.ReadLine());
+
+                    if (n <= 50)
+                    {
+                        sal = n * 10;
+
+                        Console.WriteLine("Salário total: R$ " + sal + ",00");
+                        Console.WriteLine("Valor excedente: R$ 0,00");
+                    }
+                    else
+                    {
+                        e = n - 50;
+                        exc = e * 20;
+                        sal = exc + (50 * 10);
+
+                        Console.WriteLine("Salário total: R$ " + sal + ",00");
+                        Console.WriteLine("Valor excedente: R$ " + exc + ",00");
+                    }
+
+                    Console.Write("Deseja encerrar o programa (S/N)? ");
+                    res = Console.ReadLine();
+
+                } while (res != "S" && res != "s");
+
+                Console.ReadKey();
 
             Console.ReadKey();
         }
