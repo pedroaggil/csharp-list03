d4c7a2cdd5631bb451586d3bcb312cdaae317fc7
Pedro Gil <pedroaggil@gmail.com>
2023-04-10 21:25:54 -0300

    Update Program.cs

--- ex__01/Program.cs
+++ ex__01/Program.cs
@@ -1,6 +1,10 @@
-﻿// System issues
+﻿// Escreva um programa que exiba na tela em ordem crescente, apenas os números pares existentes entre 11 e 250
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ex__01
 {
@@ -8,7 +12,15 @@ namespace ex__01
     {
         static void main(string[] args)
         {
-            // Code...
+            for (int number = 11; number <= 250; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
