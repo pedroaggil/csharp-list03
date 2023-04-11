7524077788a0e6dc51abc054df2f30702a86a9d9
Pedro Gil <pedroaggil@gmail.com>
2023-04-11 01:28:26 -0300

    Atualizar o Program.cs

--- ex__07/Program.cs
+++ ex__07/Program.cs
@@ -1,4 +1,4 @@
-﻿// System issues
+﻿// Escreva um programa que exiba todos os números de 1 a 100 e a cada número que for múltiplo de 10, exiba a mensagem “MÚLTIPLO DE 10”
 
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,13 @@ namespace ex__07
     {
         static void main(string[] args)
         {
-            // Code...
+            for (int i = 1; i <= 100; i++) {
+                if (i % 10 != 0) {
+                    Console.WriteLine(i);
+                } else {
+                    Console.WriteLine("{0} - MULTIPLO DE 10", i);
+                }
+            }
 
             Console.ReadKey();
         }
